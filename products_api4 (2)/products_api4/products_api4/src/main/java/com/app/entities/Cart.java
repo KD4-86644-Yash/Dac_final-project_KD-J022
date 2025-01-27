54b5b12a3cfc1894af8630064f41e9d2ec892cb9
Yash Gupta <yashgupta00441@gmail.com>
2025-01-27 19:21:04 +0530

    Backend

---
+++ products_api4 (2)/products_api4/products_api4/src/main/java/com/app/entities/Cart.java
@@ -0,0 +1,37 @@
+package com.app.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "secure_users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Cart extends BaseEntity{
+	
+	@Column(length = 20)
+	private String Name;
+	@Column(length = 500)
+	private int quantity;
+	@Column(length = 500)
+	private int price;
+	
+	@OneToOne
+	@JoinColumn(name = "vandor_id")
+	private UserEntity userEntity;
+
+}
---
+++ products_api4 (2)/products_api4/products_api4/src/main/java/com/app/service/UserService.java
@@ -0,0 +1,11 @@
+package com.app.service;
+
+import com.app.dto.SigninRequest;
+import com.app.dto.SigninResponse;
+import com.app.entities.UserEntity;
+
+public interface UserService {
+
+	UserEntity addU(UserEntity u);
+	//add signup method
+}

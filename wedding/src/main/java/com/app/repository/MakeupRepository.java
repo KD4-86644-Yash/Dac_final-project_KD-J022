2d8eb45031594015fbde329a103bff1afd73ed6f
ANSH M JAISWAL <128565906+ansh-19-may@users.noreply.github.com>
2025-02-09 23:32:03 +0530

    cart post method for invitation service

--- wedding/src/main/java/com/app/repository/MakeupRepository.java
+++ wedding/src/main/java/com/app/repository/MakeupRepository.java
@@ -1,9 +1,13 @@
 package com.app.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.app.entities.MakeUp;
 
-public interface MakeUpRepository extends JpaRepository<MakeUp, Long>{
+public interface MakeupRepository extends JpaRepository<MakeUp, Long>{
+
+	List<MakeUp> findByUserEntityId(Long vendorId);
 
 }

420089c8c630dfab494a8e5c01a189a08b0fe494
ANSH M JAISWAL <128565906+ansh-19-may@users.noreply.github.com>
2025-02-10 01:11:59 +0530

    added get method for venue

--- wedding/src/main/java/com/app/service/VenueService.java
+++ wedding/src/main/java/com/app/service/VenueService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.app.dto.CartDTO;
 import com.app.dto.VenueDto;
+import com.app.entities.Mehandi;
 import com.app.entities.Venue;
 import com.app.responseapi.ApiResponse;
 
@@ -12,7 +13,7 @@ public interface VenueService {
 	List<Venue> getAllVenue();
 	ApiResponse addProduct(VenueDto dto);
 	
-	
+	Venue getSingleMehandiRecord(Long serviceId);
 	ApiResponse addVenueServiceToCart(CartDTO cartDto,Long serviceId, Long UserId);
 
 }
